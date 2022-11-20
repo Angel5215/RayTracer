f80d734054ee48971f603074e32dafcecdaf18d0
Ángel Vázquez <angel_betta@outlook.com>
2022-11-20 15:27:20 -0600

    Initial Commit

---
+++ Tests/RayTracerTests/RayTracerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RayTracer
+
+final class RayTracerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RayTracer().text, "Hello, World!")
+    }
+}
