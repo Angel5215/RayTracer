5ab214b3d8b00e8aef33e6843088fc7b11804782
Ángel Vázquez <angel_betta@outlook.com>
2022-11-21 02:24:41 -0600

    Remove preconditionFailure calls

--- Sources/RayTracer/Tuple+Operations.swift
+++ Sources/RayTracer/Tuple+Operations.swift
@@ -4,10 +4,7 @@
 
 extension Tuple {
     public func adding(_ tuple: Tuple) -> Tuple {
-        guard !(isPoint && tuple.isPoint) else {
-            preconditionFailure("Illegal operation. Both tuples can't be points simultaneously.")
-        }
-        return Tuple(
+        Tuple(
             x: x + tuple.x,
             y: y + tuple.y,
             z: z + tuple.z,
@@ -16,10 +13,7 @@ extension Tuple {
     }
 
     public func subtracting(_ tuple: Tuple) -> Tuple {
-        guard !(isVector && tuple.isPoint) else {
-            preconditionFailure("Illegal operation. Can't subtract point from vector.")
-        }
-        return Tuple(
+        Tuple(
             x: x - tuple.x,
             y: y - tuple.y,
             z: z - tuple.z,
