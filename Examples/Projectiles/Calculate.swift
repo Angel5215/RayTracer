e7b0e30116184480ff255de59d203d6fb58000be
Ángel Vázquez <angel_betta@outlook.com>
2022-12-01 02:08:13 -0600

    Extract `Calculate` command to its own file

---
+++ Examples/Projectiles/Calculate.swift
@@ -0,0 +1,35 @@
+//
+// Created by Ángel Vázquez on 01/12/22.
+//
+
+import ArgumentParser
+import Foundation
+import RayTracer
+
+struct Calculate: ParsableCommand {
+    static let configuration = CommandConfiguration(abstract: "Calculate values from firing a projectile.")
+
+    @OptionGroup var options: Options
+
+    @Option(name: .long, help: "Maximum fraction digits to display")
+    var maximumFractionDigits = 4
+
+    func run() throws {
+        let dataInput = try loadData(from: options.inputFile)
+        let (projectile, environment) = extractInformation(from: dataInput)
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = maximumFractionDigits
+
+        let positions = positionsBeforeLanding(of: projectile, in: environment)
+        for (index, point) in zip(positions.indices, positions) {
+            let formattedPoint = format(point: point, using: formatter)
+            print(index + 1, formattedPoint, separator: ". ")
+        }
+    }
+
+    private func format(point: Point, using formatter: NumberFormatter) -> String {
+        let x = formatter.string(from: NSNumber(value: point.x))!
+        let y = formatter.string(from: NSNumber(value: point.y))!
+        return "(\(x), \(y))"
+    }
+}
--- Examples/Projectiles/Command.swift
+++ Examples/Projectiles/Command.swift
@@ -21,31 +21,3 @@ struct Options: ParsableArguments {
     @Option(name: .customLong("input"), help: Help.inputFile, transform: URL.init(fileURLWithPath:))
     var inputFile: URL
 }
-
-struct Calculate: ParsableCommand {
-    static let configuration = CommandConfiguration(abstract: "Calculate values from firing a projectile.")
-
-    @OptionGroup var options: Options
-
-    @Option(name: .long, help: "Maximum fraction digits to display")
-    var maximumFractionDigits = 4
-
-    func run() throws {
-        let dataInput = try loadData(from: options.inputFile)
-        let (projectile, environment) = extractInformation(from: dataInput)
-        let formatter = NumberFormatter()
-        formatter.maximumFractionDigits = maximumFractionDigits
-
-        let positions = positionsBeforeLanding(of: projectile, in: environment)
-        for (index, point) in zip(positions.indices, positions) {
-            let formattedPoint = format(point: point, using: formatter)
-            print(index + 1, formattedPoint, separator: ". ")
-        }
-    }
-
-    private func format(point: Point, using formatter: NumberFormatter) -> String {
-        let x = formatter.string(from: NSNumber(value: point.x))!
-        let y = formatter.string(from: NSNumber(value: point.y))!
-        return "(\(x), \(y))"
-    }
-}
