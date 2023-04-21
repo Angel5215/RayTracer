291bdcadd262e7b9b2e21f02163b4b9ab88ae34b
Ángel Vázquez <angel_betta@outlook.com>
2023-04-21 00:43:41 -0600

    Complete assertion for test to ensure all values are set correctly for 4x4 matrices

--- Tests/RayTracerTests/MatrixTests.swift
+++ Tests/RayTracerTests/MatrixTests.swift
@@ -28,16 +28,28 @@ struct Matrix {
     }
 }
 
-class Matrix4Tests: XCTestCase {
+class MatrixTests: XCTestCase {
     func test_init_creates4x4Matrix() {
         let matrix = Matrix(dimension: 4, rows: [[1, 2, 3, 4], [5.5, 6.5, 7.5, 8.5], [9, 10, 11, 12], [13.5, 14.5, 15.5, 16.5]])
 
         XCTAssertEqual(matrix[0, 0], 1)
+        XCTAssertEqual(matrix[0, 1], 2)
+        XCTAssertEqual(matrix[0, 2], 3)
         XCTAssertEqual(matrix[0, 3], 4)
+
         XCTAssertEqual(matrix[1, 0], 5.5)
+        XCTAssertEqual(matrix[1, 1], 6.5)
         XCTAssertEqual(matrix[1, 2], 7.5)
+        XCTAssertEqual(matrix[1, 3], 8.5)
+
+        XCTAssertEqual(matrix[2, 0], 9)
+        XCTAssertEqual(matrix[2, 1], 10)
         XCTAssertEqual(matrix[2, 2], 11)
+        XCTAssertEqual(matrix[2, 3], 12)
+
         XCTAssertEqual(matrix[3, 0], 13.5)
+        XCTAssertEqual(matrix[3, 1], 14.5)
         XCTAssertEqual(matrix[3, 2], 15.5)
+        XCTAssertEqual(matrix[3, 3], 16.5)
     }
 }
