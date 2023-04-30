2e3e4e3be36fc9af5ffa4c8857394d4fdbcc4826
Ángel Vázquez <angel_betta@outlook.com>
2023-04-29 23:56:07 -0600

    Add two more matrices to test to improve coverage of inverse matrix calculation

--- Tests/RayTracerTests/MatrixTests.swift
+++ Tests/RayTracerTests/MatrixTests.swift
@@ -250,15 +250,31 @@ class MatrixTests: XCTestCase {
     }
 
     func test_inverted_producesInverseMatrixCorrectly() {
-        let matrix = Matrix4(values: [-5, 2, 6, -8, 1, -5, 1, 8, 7, 7, -6, -7, 1, -3, 7, 4])
-
-        let inverseMatrix = matrix.inverted()
-
-        XCTAssertEqual(inverseMatrix, Matrix4(values: [
+        let matrixA = Matrix4(values: [-5, 2, 6, -8, 1, -5, 1, 8, 7, 7, -6, -7, 1, -3, 7, 4])
+        let inverseMatrixA = matrixA.inverted()
+        XCTAssertEqual(inverseMatrixA, Matrix4(values: [
             0.21805, 0.45113, 0.24060, -0.04511,
             -0.80827, -1.45677, -0.44361, 0.52068,
             -0.07895, -0.22368, -0.05263, 0.19737,
             -0.52256, -0.81391, -0.30075, 0.30639
+        ]))
+
+        let matrixB = Matrix4(values: [8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4])
+        let inverseMatrixB = matrixB.inverted()
+        XCTAssertEqual(inverseMatrixB, Matrix4(values: [
+            -0.15385, -0.15385, -0.28205, -0.53846,
+            -0.07692, 0.12308, 0.02564, 0.03077,
+            0.35897, 0.35897, 0.43590, 0.92308,
+            -0.69231, -0.69231, -0.76923, -1.92308
+        ]))
+
+        let matrixC = Matrix4(values: [9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2])
+        let inverseMatrixC = matrixC.inverted()
+        XCTAssertEqual(inverseMatrixC, Matrix4(values: [
+            -0.04074, -0.07778, 0.14444, -0.22222,
+            -0.07778, 0.03333, 0.36667, -0.33333,
+            -0.02901, -0.14630, -0.10926, 0.12963,
+            0.17778, 0.06667, -0.26667, 0.33333
         ]))
     }
 
