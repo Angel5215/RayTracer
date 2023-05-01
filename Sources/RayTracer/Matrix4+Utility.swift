dbc4d759e240ddfe4de64f1e60ec73bf6f509cce
Ángel Vázquez <angel_betta@outlook.com>
2023-04-30 19:02:57 -0600

    Add helper factory method to create Translation matrices

---
+++ Sources/RayTracer/Matrix4+Utility.swift
@@ -0,0 +1,5 @@
+// Created on 30/04/23. Last modified in 2023.
+
+public func translation(x: Double, y: Double, z: Double) -> Matrix4 {
+    Matrix4.translation(x: x, y: y, z: z)
+}
--- Tests/RayTracerTests/MatrixTransformationsTests.swift
+++ Tests/RayTracerTests/MatrixTransformationsTests.swift
@@ -5,28 +5,28 @@ import XCTest
 
 final class MatrixTransformationsTests: XCTestCase {
     func test_translationMatrix_multiplyingPointTranslatesPoint() {
-        let translation = Matrix4.translation(x: 5, y: -3, z: 2)
+        let translationMatrix = translation(x: 5, y: -3, z: 2)
         let initialPoint = point(x: -3, y: 4, z: 5)
 
-        let result = translation * initialPoint
+        let result = translationMatrix * initialPoint
 
         XCTAssertEqual(result, point(x: 2, y: 1, z: 7))
     }
 
     func test_translationMatrix_multiplyingInverseByPointTranslatesPointInReverse() {
-        let translation = Matrix4.translation(x: 5, y: -3, z: 2)
+        let translationMatrix = translation(x: 5, y: -3, z: 2)
         let initialPoint = point(x: -3, y: 4, z: 5)
 
-        let result = translation.inverted()! * initialPoint
+        let result = translationMatrix.inverted()! * initialPoint
 
         XCTAssertEqual(result, point(x: -8, y: 7, z: 3))
     }
 
     func test_translationMatrix_multiplyingVectorDoesNotAffectVector() {
-        let translation = Matrix4.translation(x: 5, y: -3, z: 2)
+        let translationMatrix = translation(x: 5, y: -3, z: 2)
         let initialVector = vector(x: -3, y: 4, z: 5)
 
-        let result = translation * initialVector
+        let result = translationMatrix * initialVector
 
         XCTAssertEqual(result, initialVector)
     }
