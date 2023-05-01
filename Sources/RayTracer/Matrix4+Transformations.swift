915c8c25876f099d9d2acf34edc1da74ca257d9a
Ángel Vázquez <angel_betta@outlook.com>
2023-04-30 19:00:19 -0600

    Implement translation matrices

---
+++ Sources/RayTracer/Matrix4+Transformations.swift
@@ -0,0 +1,13 @@
+// Created on 30/04/23. Last modified in 2023.
+
+import Foundation
+
+public extension Matrix4 {
+    static func translation(x: Double, y: Double, z: Double) -> Self {
+        var translationMatrix = identity
+        translationMatrix[0, 3] = x
+        translationMatrix[1, 3] = y
+        translationMatrix[2, 3] = z
+        return translationMatrix
+    }
+}
