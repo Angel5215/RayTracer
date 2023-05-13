1fb600dafb1b61c96f3f739c34d2d5c96360c5bb
Ángel Vázquez <angel_betta@outlook.com>
2023-05-13 14:10:39 -0600

    Compute position of a ray given a distance

--- Tests/RayTracerTests/RayTests.swift
+++ Tests/RayTracerTests/RayTests.swift
@@ -6,6 +6,10 @@ import XCTest
 struct Ray {
     let origin: Point
     let direction: Vector
+
+    func position(at distance: Double) -> Point {
+        distance * direction + origin
+    }
 }
 
 final class RayTests: XCTestCase {
@@ -14,5 +18,14 @@ final class RayTests: XCTestCase {
 
         XCTAssertEqual(ray.origin, point(x: 1, y: 2, z: 3))
         XCTAssertEqual(ray.direction, vector(x: 4, y: 5, z: 6))
+    }
+
+    func test_position_computesPointAtGivenDistance() {
+        let ray = Ray(origin: point(x: 2, y: 3, z: 4), direction: vector(x: 1, y: 0, z: 0))
+
+        XCTAssertEqual(ray.position(at: 0), point(x: 2, y: 3, z: 4))
+        XCTAssertEqual(ray.position(at: 1), point(x: 3, y: 3, z: 4))
+        XCTAssertEqual(ray.position(at: -1), point(x: 1, y: 3, z: 4))
+        XCTAssertEqual(ray.position(at: 2.5), point(x: 4.5, y: 3, z: 4))
     }
 }
